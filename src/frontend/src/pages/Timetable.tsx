3bf49fcdf3c8bd72e7483ad64d8e4bd2f412a318
Daniel Nellessen <dnellessen@gmx.de>
2025-05-28 22:31:42 +0200

    Add loader animation

--- src/frontend/src/pages/Timetable.tsx
+++ src/frontend/src/pages/Timetable.tsx
@@ -1,16 +1,27 @@
+// Components
+import { Loader } from "../components/ui/loader"
+
 // Cards
 import GenerateTimetable from "../components/cards/GenerateTimetable"
 import DisplayTimetable from "../components/cards/display/Timetable"
 
 function Timetable({ isLoading, data, setData }) {
-    if (!isLoading) {
+    if (isLoading) {
         return (
-            <>
-                <GenerateTimetable data={data} setData={setData} />
-                <DisplayTimetable data={data} />
-            </>
+            <div className="min-h-[calc(100vh-250px)] flex bg-gray-50 p-4 flex-nowrap justify-center items-center">
+                <div className="text-center">
+                    <Loader />
+                </div>
+            </div>
         );
     }
+
+    return (
+        <>
+            <GenerateTimetable data={data} setData={setData} />
+            <DisplayTimetable data={data} />
+        </>
+    );
 }
 
 export default Timetable;
