3484c71d3700eefc92fa2a342410b5dfe9b943ba
Daniel Nellessen <dnellessen@gmx.de>
2025-05-31 18:13:12 +0200

    Adjust content min height and add icon

--- src/frontend/src/pages/error/404.tsx
+++ src/frontend/src/pages/error/404.tsx
@@ -1,8 +1,14 @@
 
+// Icons
+import { Snail } from "lucide-react";
+
 function Error404() {
     return (
-        <div className="min-h-[calc(100vh-130px)] flex bg-gray-50 p-4 flex-nowrap justify-center items-center">
-            <div className="text-center">
+        <div className="min-h-[calc(100vh-347px)] flex bg-gray-50 p-8 flex-nowrap justify-center items-center">
+            <div className="flex flex-col group items-center ml-4">
+                <div className="hover:-scale-x-100">
+                    <Snail className="w-20 h-20" />
+                </div>
                 <h1 className="text-3xl font-bold">404</h1>
                 <h3 className="">Page not found</h3>
             </div>
