b100b92c86b183bd139c67662c617794a2be5ea2
Daniel Nellessen <dnellessen@gmx.de>
2025-06-06 20:27:21 +0200

    Update comment

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig((mode) => {
-    // Load env file based on `mode` in the current working directory.
-    // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+    // Load env file based on `mode` [development or production] in the current working directory.
     const env = loadEnv(mode, process.cwd(), 'VITE_');
 
     return {
